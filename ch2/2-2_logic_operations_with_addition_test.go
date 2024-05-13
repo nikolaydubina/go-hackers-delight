e94498471ecc80d8643fc6372d4af34cf212531a
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-05-13 14:07:56 +0800

    nit

---
+++ ch2/2-2_logic_operations_with_addition_test.go
@@ -0,0 +1,37 @@
+package ch2
+
+import "testing"
+
+func Fuzz_LogicOperationsWithAddition(f *testing.F) {
+	f.Fuzz(func(t *testing.T, x, y int32) {
+		v := []bool{
+			-x == ^x+1,                          //
+			-x == ^(x - 1),                      //
+			^x == -x-1,                          //
+			-^x == x+1,                          // addition can be implemented with not and negation
+			^-x == x-1,                          //
+			x+y == (x - ^y - 1),                 //
+			x+y == ((x ^ y) + (2 * (x & y))),    // this has carry probability of 0.25, when normally it is 0.5
+			x+y == ((x | y) + (x & y)),          //
+			x+y == ((2 * (x | y)) - (x ^ y)),    //
+			x-y == x+^y+1,                       //
+			x-y == ((x ^ y) - (2 * (^x & y))),   //
+			x-y == ((x & (^y)) - (^x & y)),      //
+			x-y == ((2 * (x &^ y)) - (x ^ y)),   //
+			x^y == ((x | y) - (x & y)),          // shows how to do XOR in RISC with only three basic instructions
+			x&^y == ((x | y) - y),               //
+			x&^y == (x - (x & y)),               //
+			^(x - y) == (y - x - 1),             //
+			^(x - y) == ^x+y,                    //
+			^(x ^ y) == ((x & y) - (x | y) - 1), // bitwise equality (===)
+			^(x ^ y) == ((x & y) + ^(x | y)),    // bitwise equality (===)
+			x|y == ((x &^ y) + y),               //
+			x&y == ((^x | y) - ^x),              //
+		}
+		for i, q := range v {
+			if !q {
+				t.Error(i, x, y)
+			}
+		}
+	})
+}
