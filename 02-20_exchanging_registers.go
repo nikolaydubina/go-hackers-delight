a6b447146a9547b541247d43f92a293567f1fa0a
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-05-17 14:43:41 +0800

    2-20

--- 02-20_exchanging_registers.go
+++ 02-20_exchanging_registers.go
@@ -1,10 +1,66 @@
 package hd
 
+type integers interface {
+	int | uint | int8 | uint8 | int16 | uint16 | int32 | uint32 | int64 | uint64
+}
+
 // ExchangeRegisters illustrates very old trick on how to exchange two registers without using third one.
 // This is swap operation. Also known as multiple assignment in Go.
-func ExchangeRegisters(x, y int32) (int32, int32) {
+func ExchangeRegisters[T integers](x, y T) (T, T) {
 	x = x ^ y
 	y = y ^ x
 	x = x ^ y
 	return x, y
+}
+
+// ExchangeRegistersMasked illustrates how to exchange only masked bits between two registers.
+// This can be done in 3 cycles, given parallelism and and-not instructions.
+func ExchangeRegistersMasked[T integers](x, y, m T) (T, T) {
+	t := (x & ^m) | (y & m)
+	y = (y & ^m) | (x & m)
+	x = t
+	return x, y
+}
+
+func ExchangeRegistersMasked2[T integers](x, y, m T) (T, T) {
+	x = x ^ y
+	y = y ^ (x & m)
+	x = x ^ y
+	return x, y
+}
+
+// ExchangeRegistersMasked3 is heavily using  bitwise equality, but in Go there is no such operator, so expanding that notation.
+func ExchangeRegistersMasked3[T integers](x, y, m T) (T, T) {
+	x = ^(x ^ y)
+	y = ^(y ^ (x | ^m))
+	x = ^(x ^ y)
+	return x, y
+}
+
+// ExchangeRegistersMasked4 also executes in three cycles, given sufficient instruction parallelism in the CPU.
+func ExchangeRegistersMasked4[T integers](x, y, m T) (T, T) {
+	t := (x ^ y) & m
+	x = x ^ t
+	y = y ^ t
+	return x, y
+}
+
+// ExchangeBitsInRegister swaps two regions of bits within single register without affecting other bits.
+// For example, swapping B and D regions. B and D should be same size, but A C E and B/D can be different sizes.
+// k is number of bits of C + D
+// md is mask for field D
+// mo is mask for fields A C and E (all fields to not swap)
+// [aaaa bbbb cccc dddd eeee] -> [aaaa dddd cccc bbbb eeee]
+// This is straight forward approach and requires eleven instructions and six cycles.
+func ExchangeBitsInRegister(x, md, mo uint32, k int) uint32 {
+	t1 := (x & md) << k
+	t2 := (x >> k) & md
+	return (x & mo) | t1 | t2
+}
+
+// ExchangeBitsInRegisterFast requires eight instructions and five cycles.
+func ExchangeBitsInRegisterFast(x, md, mo uint32, k int) uint32 {
+	t1 := (x ^ (x >> k)) & md
+	t2 := t1 << k
+	return x ^ t1 ^ t2
 }
--- 02-20_exchanging_registers_test.go
+++ 02-20_exchanging_registers_test.go
@@ -1,6 +1,7 @@
 package hd_test
 
 import (
+	"fmt"
 	"math"
 	"testing"
 
@@ -31,4 +32,79 @@ func FuzzExchangeRegisters(f *testing.F) {
 			t.Error(x, y, gx, gy)
 		}
 	})
+}
+
+func ExampleExchangeRegistersMasked() {
+	var x uint32 = 0xABCDEF12
+	var y uint32 = 0x12345678
+	var m uint32 = 0x0F0F0F0F
+	x, y = hd.ExchangeRegistersMasked(x, y, m)
+	fmt.Printf("\n%0X\n%0X", x, y)
+	// Output:
+	// A2C4E618
+	// 1B3D5F72
+}
+
+func ExampleExchangeRegistersMasked_bits() {
+	var x uint32 = 0b11110000111100001111000011110000
+	var y uint32 = 0b00001111000011110000111100001111
+	var m uint32 = 0b00001111111100000000000000000000
+
+	x, y = hd.ExchangeRegistersMasked(x, y, m)
+	fmt.Printf("\n%032b\n%032b", x, y)
+	// Output:
+	// 11111111000000001111000011110000
+	// 00000000111111110000111100001111
+}
+
+func FuzzExchangeRegistersMasked(f *testing.F) {
+	var vs = []int32{
+		0,
+		1,
+		-1,
+		math.MinInt32,
+		math.MinInt32 / 2,
+		math.MinInt32 + 1,
+		math.MaxInt32,
+		math.MaxInt32 / 2,
+		math.MaxInt32 - 1,
+	}
+	for _, x := range vs {
+		for _, y := range vs {
+			for _, m := range vs {
+				f.Add(x, y, m)
+			}
+		}
+	}
+	f.Fuzz(func(t *testing.T, x, y, m int32) {
+		ex, ey := hd.ExchangeRegistersMasked(x, y, m)
+
+		if gx, gy := hd.ExchangeRegistersMasked2(x, y, m); ex != gx || ey != gy {
+			t.Error(2, x, y, m, gx, gy)
+		}
+
+		if gx, gy := hd.ExchangeRegistersMasked3(x, y, m); ex != gx || ey != gy {
+			t.Error(3, x, y, m, gx, gy)
+		}
+
+		if gx, gy := hd.ExchangeRegistersMasked4(x, y, m); ex != gx || ey != gy {
+			t.Error(4, x, y, m, gx, gy)
+		}
+	})
+}
+
+func ExampleExchangeBitsInRegister() {
+	var xx uint32 = 0xABCDEF12
+	var mb uint32 = 0x0000F000
+	var mo uint32 = 0xF0FF0FFF
+	fmt.Printf("%0X", hd.ExchangeBitsInRegister(xx, mb, mo, 4*3))
+	// Output: AECDBF12
+}
+
+func ExampleExchangeBitsInRegisterFast() {
+	var xx uint32 = 0xABCDEF12
+	var mb uint32 = 0x0000F000
+	var mo uint32 = 0xF0FF0FFF
+	fmt.Printf("%0X", hd.ExchangeBitsInRegisterFast(xx, mb, mo, 4*3))
+	// Output: AECDBF12
 }
