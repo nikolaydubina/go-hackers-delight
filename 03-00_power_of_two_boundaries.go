f0cfa3b142d5cc79c2626fc06de3b525f3ce0389
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-05-18 13:24:21 +0800

    ch3

---
+++ 03-00_power_of_two_boundaries.go
@@ -0,0 +1,75 @@
+package hd
+
+type unsigned interface {
+	uint | uint8 | uint16 | uint32 | uint64
+}
+
+func RoundDownBlockPowerOfTwo[T unsigned](x T, k int) T { return x & (-T(1) << k) }
+
+func RoundDownBlockPowerOfTwo2[T unsigned](x T, k int) T { return (x >> k) << k }
+
+// RoundUpBlockPowerOfTwo where k is log2 of power of two you want to round to.
+func RoundUpBlockPowerOfTwo[T unsigned](x T, k int) T {
+	var t T = (1 << k) - 1 // if k is const, this is const too
+	return (x + t) & ^t
+}
+
+func RoundUpBlockPowerOfTwo2[T unsigned](x T, k int) T {
+	var t T = -T(1) << k // if k is const, this is const too
+	return (x - t - 1) & t
+}
+
+// FLPTwo is floor to nearest power of two. Values >= 2^31 will be zero.
+func FLPTwo(x uint32) uint32 { return 1 << (31 - NLZ(x)) }
+
+func FLPTwo2(x uint32) uint32 { return 1 << (NLZ(x) ^ 31) }
+
+func FLPTwo3(x uint32) uint32 { return 0x80000000 >> NLZ(x) }
+
+// FLPTwo4 is alternative branch-free version when NLZ is not available.
+func FLPTwo4(x uint32) uint32 {
+	x = x | (x >> 1)
+	x = x | (x >> 2)
+	x = x | (x >> 4)
+	x = x | (x >> 8)
+	x = x | (x >> 16)
+	return x - (x >> 1)
+}
+
+// FLPTwo5 uses simple loop that executes in 4 * NLZ(x) + 3 instructions.
+func FLPTwo5(x uint32) uint32 {
+	var y uint32 = 0x80000000
+	for y > x {
+		y >>= 1
+	}
+	return y
+}
+
+// CLPTwo is ceil to nearest power of two. Values >= 2^31 will be zero.
+func CLPTwo(x uint32) uint32 { return 1 << (32 - NLZ(x-1)) }
+
+func CLPTwo2(x uint32) uint32 { return 0x80000000 >> (NLZ(x-1) - 1) }
+
+// CLPTwo3 is alternative branch-free version when NLZ is not available.
+func CLPTwo3(x uint32) uint32 {
+	x = x - 1
+	x = x | (x >> 1)
+	x = x | (x >> 2)
+	x = x | (x >> 4)
+	x = x | (x >> 8)
+	x = x | (x >> 16)
+	return x + 1
+}
+
+// IsPowerOfTwoBoundaryCrossed checks if adding l to a crosses power of two boundary of block-size b.
+// This and versions bellow are five or six RISC instructions.
+// b has to be power of two and likely to be a constant.
+func IsPowerOfTwoBoundaryCrossed(a, l, b uint32) bool { return -(a | -b) < l }
+
+func IsPowerOfTwoBoundaryCrossed2(a, l, b uint32) bool { return (^(a | -b) + 1) < l }
+
+func IsPowerOfTwoBoundaryCrossed3(a, l, b uint32) bool { return ((^a & (b - 1)) + 1) < l }
+
+func IsPowerOfTwoBoundaryCrossed4(a, l, b uint32) bool { return (8 - (a & (b - 1))) < l }
+
+// TODO: round power of two signed
---
+++ 03-00_power_of_two_boundaries_test.go
@@ -0,0 +1,143 @@
+package hd_test
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	hd "github.com/nikolaydubina/go-hackers-delight"
+)
+
+func ExampleRoundDownBlockPowerOfTwo() {
+	fmt.Println(hd.RoundDownBlockPowerOfTwo[uint](257, 8))
+	// Output: 256
+}
+
+func ExampleRoundUpBlockPowerOfTwo() {
+	fmt.Println(hd.RoundUpBlockPowerOfTwo[uint](210, 8))
+	// Output: 256
+}
+
+func ExampleFLPTwo() {
+	fmt.Println(hd.FLPTwo(310))
+	// Output: 256
+}
+
+func ExampleCLPTwo() {
+	fmt.Println(hd.CLPTwo(200))
+	// Output: 256
+}
+
+func ExampleIsPowerOfTwoBoundaryCrossed_crossed() {
+	fmt.Println(hd.IsPowerOfTwoBoundaryCrossed(100, 300, 256))
+	// Output: true
+}
+
+func ExampleIsPowerOfTwoBoundaryCrossed_not_crossed() {
+	fmt.Println(hd.IsPowerOfTwoBoundaryCrossed(100, 100, 256))
+	// Output: false
+}
+
+func FuzzRoundBlockPowerOfTwo(f *testing.F) {
+	var vs = []uint32{
+		0,
+		1,
+		math.MaxUint32,
+		math.MaxUint32 / 2,
+		math.MaxUint32 - 1,
+	}
+	for p := range 32 {
+		for _, x := range vs {
+			f.Add(x, p)
+		}
+	}
+	f.Fuzz(func(t *testing.T, x uint32, p int) {
+		if p <= 0 || p >= 32 {
+			t.Skip()
+		}
+
+		// simple algo to get boundaries
+		// it is actually naive version of code that we are testing, but is easier to follow
+		l := x - (x % (1 << p))
+		h := l + (1 << p)
+		if (x % (1 << p)) == 0 {
+			h = x
+		}
+
+		vs := []struct {
+			exp uint32
+			got uint32
+		}{
+			{l, hd.RoundDownBlockPowerOfTwo(x, p)},
+			{l, hd.RoundDownBlockPowerOfTwo2(x, p)},
+			{h, hd.RoundUpBlockPowerOfTwo(x, p)},
+			{h, hd.RoundUpBlockPowerOfTwo2(x, p)},
+		}
+		for i, v := range vs {
+			if v.got != v.exp {
+				t.Error(i, x, p, "exp", v.exp, "got", v.got)
+			}
+		}
+	})
+}
+
+func FuzzRoundToPowerOfTwo(f *testing.F) {
+	var vs = []uint32{
+		0,
+		1,
+		math.MaxInt32,
+		math.MaxInt32 / 2,
+		math.MaxInt32 - 1,
+		math.MaxUint32,
+		math.MaxUint32 / 2,
+		math.MaxUint32 - 1,
+	}
+	for _, x := range vs {
+		f.Add(x)
+	}
+
+	// pre-computed powers of two possible in int32
+	powers := [...]uint32{
+		0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096,
+		8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576,
+		2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728,
+		268435456, 536870912, 1073741824, 2147483648,
+		// by design skip >= 2^31
+		//4294967295,
+	}
+
+	f.Fuzz(func(t *testing.T, x uint32) {
+		// definition
+		var l, h uint32
+		if x > 0 {
+			l = powers[0]
+			for _, p := range powers {
+				if p <= x {
+					l = p
+				}
+				if h == 0 && x <= p {
+					h = p
+				}
+			}
+		}
+
+		vs := []struct {
+			exp uint32
+			got uint32
+		}{
+			{l, hd.FLPTwo(x)},
+			{l, hd.FLPTwo2(x)},
+			{l, hd.FLPTwo3(x)},
+			{l, hd.FLPTwo4(x)},
+			{l, hd.FLPTwo5(x)},
+			{h, hd.CLPTwo(x)},
+			{h, hd.CLPTwo2(x)},
+			{h, hd.CLPTwo3(x)},
+		}
+		for i, v := range vs {
+			if v.got != v.exp {
+				t.Error(i, "x", x, "exp", v.exp, "got", v.got)
+			}
+		}
+	})
+}
