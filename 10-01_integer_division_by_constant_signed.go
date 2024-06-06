072555f32c103772ab09b595a2585b2e172e47df
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-06-06 12:17:13 +0800

    cleanup

--- 10-01_integer_division_by_constant_signed.go
+++ 10-01_integer_division_by_constant_signed.go
@@ -8,10 +8,10 @@ import (
 // This is illustration, for performance k should be fixed at compile time.
 // This is four branch-free instructions.
 func DivSignedPowTwo(n int32, k int) int32 {
-	t := n >> (k - 1)                // form the integer
-	t = int32(uint32(t) >> (32 - k)) // shift right unsigned is not available directly in Go, so simulating with conversion to uint32, 2 ** k - 1 if n < 0 else 0.
-	t += n                           // Add it to n,
-	return t >> k                    // and shift right (signed).
+	t := n >> (k - 1)
+	t = int32(uint32(t) >> (32 - k))
+	t += n
+	return t >> k
 }
 
 func DivSignedPowTwo_fixed5(n int32) int32 {
@@ -41,32 +41,32 @@ func DivModSignedPowTwo(n int32, k int) (q, r int32) {
 // DivModSignedThree this computes in 6 instructions.
 // This is 9 or 10 cycles. Meanwhile, divide operation can take 20 cycles.
 func DivModSignedThree(n int32) (q, r int32) {
-	const M int32 = 0x5555_5556       // Magic (2 ** 32 + 2) / 3
-	q = MultiplyHighOrderSigned(M, n) // (mulhs), multiply signed, q = floor(M*n/2**32), this can be a single instruction on many architectures.
-	t := int32(uint32(n) >> 31)       // shift right unsigned is not available in Go, so simulating with conversion to uint32, Add 1 to q if
-	q += t                            // n is negative.
+	const M int32 = 0x5555_5556
+	q = MultiplyHighOrderSigned(M, n)
+	t := int32(uint32(n) >> 31)
+	q += t
 	return q, n - (q * 3)
 }
 
 // DivModSignedFive is similar to DivMod3, but error terms is too large, and thus it needs slight variation of magic constant and correction with shift right.
 func DivModSignedFive(n int32) (q, r int32) {
-	const M int32 = 0x6666_6667       // Magic (2 ** 33 + 3) / 5
-	q = MultiplyHighOrderSigned(M, n) // (mulhs), multiply signed, q = floor(M*n/2**32), this can be a single instruction on many architectures.
+	const M int32 = 0x6666_6667
+	q = MultiplyHighOrderSigned(M, n)
 	q >>= 1
-	t := int32(uint32(n) >> 31) // shift right unsigned is not available in Go, so simulating with conversion to uint32, Add 1 to q if negative
-	q += t                      // n is negative.
+	t := int32(uint32(n) >> 31)
+	q += t
 	return q, n - (q * 5)
 }
 
 // DivModSignedSeven is similar to DivMod3, but error terms is too large, and thus it needs slight variation of magic constant and correction with shift right.
 // The magic is still too large, and thus it needs flipping sign and addition.
 func DivModSignedSeven(n int32) (q, r int32) {
-	const M int32 = -1840700269       // Magic (2 ** 34 + 5) / 7 - 2 ** 32. Original text uses 0x92492493, but this gives wrong values and is different from formula.
-	q = MultiplyHighOrderSigned(M, n) // (mulhs), multiply signed, q = floor(M*n/2**32), this can be a single instruction on many architectures.
+	const M int32 = -1840700269
+	q = MultiplyHighOrderSigned(M, n)
 	q += n
 	q >>= 2
-	t := int32(uint32(n) >> 31) // shift right unsigned is not available in Go, so simulating with conversion to uint32, Add 1 to q if negative
-	q += t                      // n is negative.
+	t := int32(uint32(n) >> 31)
+	q += t
 	return q, n - (q * 7)
 }
 
