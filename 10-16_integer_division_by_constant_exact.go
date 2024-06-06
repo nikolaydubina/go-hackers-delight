7866746f527b177e879b9ba8a56244e65d1fdb53
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-06-06 16:43:52 +0800

    ch10-16-div-exact

--- 10-16_integer_division_by_constant_exact.go
+++ 10-16_integer_division_by_constant_exact.go
@@ -1,8 +1,12 @@
 package hd
 
-// DivModExactSeven uses multiplicative inverse. It expects exact division.
-// Magic (5 * 2 ** 32 + 1) / 7 = binary 0xB6DB_6DB7
-func DivModExactSeven(n int32) (q, r int32) { return -0x49249249 * n, 0 }
+func DivExactSeven(n int32) int32 { return -0x49249249 * n }
+
+// DivExact uses multiplicative inverse that can be computed at compile time.
+func DivExact(n, d int32) int32 {
+	var M int32 = MultiplicativeInverseNewtonInt(d)
+	return M * n
+}
 
 func MultiplicativeInverseEuclidInt(d int32) int32 {
 	// main algorithm works with by interpreting bits of inputs and outputs as uint32
@@ -22,4 +26,24 @@ func MultiplicativeInverseEuclid(d uint32) uint32 {
 		x1, x2, v1, v2 = x2, x3, v2, v3
 	}
 	return x2
+}
+
+func MultiplicativeInverseNewtonInt(d int32) int32 {
+	// main algorithm works with by interpreting bits of inputs and outputs as uint32
+	return int32(MultiplicativeInverseNewton(uint32(d)))
+}
+
+// MultiplicativeInverseNewton uses Newton method of multiplicative inverse.
+// It follows from the wellknown fact that sequence of xn_1 = xn * (2 - d * xn) converges to 1/d (mod d) given good starting xn.
+// This also works with any inverse module of power of 2.
+// Each iteration doubles number of correct bits.
+func MultiplicativeInverseNewton(d uint32) uint32 {
+	if (d % 2) == 0 {
+		panic("even number")
+	}
+	for xn := d; ; xn = xn * (2 - d*xn) {
+		if (d * xn) == 1 {
+			return xn
+		}
+	}
 }
--- 10-16_integer_division_by_constant_exact_test.go
+++ 10-16_integer_division_by_constant_exact_test.go
@@ -16,8 +16,8 @@ func FuzzDivModExactSeven(f *testing.F) {
 		if r != 0 {
 			t.Skip()
 		}
-		if gotQ, gotR := hd.DivModExactSeven(x); q != gotQ || r != gotR {
-			t.Errorf("DivModExactSeven(%d) = got(%d, %d); want (%d, %d)", x, gotQ, gotR, q, r)
+		if gotQ := hd.DivExactSeven(x); q != gotQ {
+			t.Errorf("DivExactSeven(%d) = %d; want %d", x, gotQ, q)
 		}
 	})
 }
@@ -27,7 +27,61 @@ func ExampleMultiplicativeInverseEuclidInt() {
 	// Output: -49249249
 }
 
-func ExampleMultiplicativeInverseEuclid() {
-	fmt.Printf("%X", hd.MultiplicativeInverseEuclid(7))
-	// Output: B6DB6DB7
+func uint32FromHex(x int32) uint32 { return uint32(x) }
+
+func TestMultiplicativeInverse(t *testing.T) {
+	tests := []struct {
+		d uint32
+		i uint32
+	}{
+		{d: uint32FromHex(-7), i: 0x4924_9249},
+		{d: uint32FromHex(-5), i: 0x3333_3333},
+		{d: uint32FromHex(-3), i: 0x5555_5555},
+		{d: uint32FromHex(-1), i: 0xFFFF_FFFF},
+		{d: 1, i: 1},
+		{d: 3, i: 0xAAAA_AAAB},
+		{d: 5, i: 0xCCCC_CCCD},
+		{d: 7, i: 0xB6DB_6DB7},
+		{d: 9, i: 0x38E3_8E39},
+		{d: 11, i: 0xBA2E_8BA3},
+		{d: 13, i: 0xC4EC_4EC5},
+		{d: 15, i: 0xEEEE_EEEF},
+		{d: 25, i: 0xC28F_5C29},
+		{d: 125, i: 0x26E9_78D5},
+		{d: 625, i: 0x3AFB_7E91},
+	}
+	for _, tt := range tests {
+		vs := []uint32{
+			hd.MultiplicativeInverseEuclid(tt.d),
+			hd.MultiplicativeInverseNewton(tt.d),
+		}
+		for i, v := range vs {
+			if tt.i != v {
+				t.Errorf("d(%d) = %d; want %d; method %d", tt.d, v, tt.i, i)
+			}
+		}
+	}
+}
+
+func FuzzDivExact(f *testing.F) {
+	for _, x := range fuzzInt32 {
+		for _, d := range fuzzInt32 {
+			f.Add(x, d)
+		}
+	}
+	f.Fuzz(func(t *testing.T, x, d int32) {
+		if d == 0 {
+			t.Skip()
+		}
+		if d%2 == 0 {
+			t.Skip()
+		}
+		q, r := x/d, x%d
+		if r != 0 {
+			t.Skip()
+		}
+		if gotQ := hd.DivExact(x, d); q != gotQ {
+			t.Errorf("DivExact(%d) = %d; want %d", x, gotQ, q)
+		}
+	})
 }
