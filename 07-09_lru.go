af38ad60f584dbc833720f57dfc354b79669017c
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-06-06 17:34:11 +0800

    ch07-fuzz-lru-cache

--- 07-09_lru.go
+++ 07-09_lru.go
@@ -5,14 +5,14 @@ package hd
 // Least significant byte holds row 0.
 // The whole structure fits into single 64bit register.
 // Line is is references by i-th byte of the register.
-type LRUCache uint64
+type LRUCache struct{ m uint64 }
 
 // Add line i to the cache.
 // This is five or six instructions on 64bit RISC.
 // Values of i should be in [0, 7].
-func (m *LRUCache) Add(i uint8) {
-	*m = *m | (0xFF << (8 * i))
-	*m = *m & ^(0x0101_0101_0101_0101 << i)
+func (c *LRUCache) Add(i uint8) {
+	c.m |= (0xFF << (8 * i))
+	c.m &= ^(0x0101_0101_0101_0101 << i)
 }
 
-func (m *LRUCache) LeastRecentlyUsed() uint8 { return 7 - uint8(ZByteL64(uint64(*m))) }
+func (c *LRUCache) LeastRecentlyUsed() uint8 { return uint8(7 - ZByteL64(uint64(c.m))) }
--- 07-09_lru_test.go
+++ 07-09_lru_test.go
@@ -27,3 +27,46 @@ func TestLRUCache(t *testing.T) {
 		t.Errorf("expected %d, got %d: cache=%032b", 1, cache.LeastRecentlyUsed(), cache)
 	}
 }
+
+func FuzzLRUCache(f *testing.F) {
+	var cache hd.LRUCache
+	cacheBasic := NewLRUCacheBasic()
+
+	count := 0
+	f.Fuzz(func(t *testing.T, x uint8) {
+		x = x % 8
+		count++
+
+		cache.Add(x)
+		cacheBasic.Add(x)
+
+		// fill out the cache
+		if count < 8 {
+			return
+		}
+
+		if cache.LeastRecentlyUsed() != cacheBasic.LeastRecentlyUsed() {
+			t.Errorf("x=%v expected %d, got %d: cache=%016x cache_basic=%v", x, cacheBasic.LeastRecentlyUsed(), cache.LeastRecentlyUsed(), cache, cacheBasic)
+		}
+	})
+}
+
+// LRUCacheBasic is reference implementation that uses array and simple movement of all elements.
+type LRUCacheBasic struct {
+	vals [8]uint8
+}
+
+func NewLRUCacheBasic() *LRUCacheBasic { return &LRUCacheBasic{vals: [8]uint8{0, 1, 2, 3, 4, 5, 6, 7}} }
+
+func (m *LRUCacheBasic) Add(i uint8) {
+	i = i % 8
+	for j, v := range m.vals {
+		if v == i {
+			copy(m.vals[1:j+1], m.vals[:j])
+			m.vals[0] = i
+			return
+		}
+	}
+}
+
+func (m *LRUCacheBasic) LeastRecentlyUsed() uint8 { return m.vals[len(m.vals)-1] }
