c8cad8fb99f157f93c8506cfdb2e4ccc67c6e3b2
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-06-07 16:26:25 +0800

    nit

--- 04-03_arithmetic_bounds_propagating_logical.go
+++ 04-03_arithmetic_bounds_propagating_logical.go
@@ -3,16 +3,15 @@ package hd
 // MinOR is min(x|y) for x in [minx, maxx] and y in [miny, maxy].
 // This gives tighter bound than max(minx, miny).
 func MinOR(minx, maxx, miny, maxy uint32) uint32 {
-	var temp uint32
 	for m := uint32(0x8000_0000); m != 0; m >>= 1 {
 		if (^minx & miny & m) != 0 {
-			if temp = (minx | m) & -m; temp <= maxx {
+			if temp := (minx | m) & -m; temp <= maxx {
 				minx = temp
 				break
 			}
 		} else {
 			if (minx & ^miny & m) != 0 {
-				if temp = (miny | m) & -m; temp <= maxy {
+				if temp := (miny | m) & -m; temp <= maxy {
 					miny = temp
 					break
 				}
@@ -25,14 +24,13 @@ func MinOR(minx, maxx, miny, maxy uint32) uint32 {
 // MaxOR is max(x|y) for x in [minx, maxx] and y in [miny, maxy].
 // This gives tighter bound than maxx + maxy.
 func MaxOR(minx, maxx, miny, maxy uint32) uint32 {
-	var temp uint32
 	for m := uint32(0x8000_0000); m != 0; m >>= 1 {
 		if (maxx & maxy & m) != 0 {
-			if temp = (maxx - m) | (m - 1); temp >= minx {
+			if temp := (maxx - m) | (m - 1); temp >= minx {
 				maxx = temp
 				break
 			}
-			if temp = (maxy - m) | (m - 1); temp >= miny {
+			if temp := (maxy - m) | (m - 1); temp >= miny {
 				maxy = temp
 				break
 			}
@@ -44,14 +42,13 @@ func MaxOR(minx, maxx, miny, maxy uint32) uint32 {
 // MinAND is min(x&y) for x in [minx, maxx] and y in [miny, maxy].
 // This gives tighter bound than 0.
 func MinAND(minx, maxx, miny, maxy uint32) uint32 {
-	var temp uint32
 	for m := uint32(0x8000_0000); m != 0; m >>= 1 {
 		if (^minx & ^miny & m) != 0 {
-			if temp = (minx | m) & -m; temp <= maxx {
+			if temp := (minx | m) & -m; temp <= maxx {
 				minx = temp
 				break
 			}
-			if temp = (miny | m) & -m; temp <= maxy {
+			if temp := (miny | m) & -m; temp <= maxy {
 				miny = temp
 				break
 			}
@@ -63,16 +60,15 @@ func MinAND(minx, maxx, miny, maxy uint32) uint32 {
 // MaxAND is max(x|y) for x in [minx, maxx] and y in [miny, maxy].
 // This gives tighter bound than min(maxx, maxy).
 func MaxAND(minx, maxx, miny, maxy uint32) uint32 {
-	var temp uint32
 	for m := uint32(0x8000_0000); m != 0; m >>= 1 {
 		if (maxx & ^maxy & m) != 0 {
-			if temp = (maxx & ^m) | (m - 1); temp >= minx {
+			if temp := (maxx & ^m) | (m - 1); temp >= minx {
 				maxx = temp
 				break
 			}
 		} else {
 			if (^maxx & maxy & m) != 0 {
-				if temp = (maxy & ^m) | (m - 1); temp >= miny {
+				if temp := (maxy & ^m) | (m - 1); temp >= miny {
 					maxy = temp
 					break
 				}
