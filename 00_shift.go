2c647409ea67774229e796040222239eebd4f0ab
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-06-08 10:58:03 +0800

    nit

--- 00_shift.go
+++ 00_shift.go
@@ -2,8 +2,8 @@ package hd
 
 // ShiftRightUnsigned32 does logical shift right that Go is missing.
 // This compiles to single RISC instruction and is going to be inlined.
-func ShiftRightUnsigned32(x int32, v int) int32 { return int32(uint32(x) >> v) }
+func ShiftRightUnsigned32[T int32 | uint32](x T, v int) T { return T(uint32(x) >> v) }
 
 // ShiftRightUnsigned64 does logical shift right that Go is missing.
 // This compiles to single RISC instruction and is going to be inlined.
-func ShiftRightUnsigned64(x int64, v int) int64 { return int64(uint64(x) >> v) }
+func ShiftRightUnsigned64[T int64 | uint64](x T, v int) T { return T(uint64(x) >> v) }
