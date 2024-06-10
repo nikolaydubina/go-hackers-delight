f2fd18e8d9f799bd793ffcdddfbacfca2bd837cf
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-06-10 12:29:08 +0800

    unsigned

--- 10-19_integer_division_by_constant_reminder.go
+++ 10-19_integer_division_by_constant_reminder.go
@@ -42,3 +42,49 @@ func Mod3Unsigned4(n uint32) uint32 {
 	n = (n >> 4) + (n & 0x000F)  // Max 0x3D
 	return uint32(mod3_unsigned_4[n])
 }
+
+// Mod5Unsigned (remu5) is using CountOnes (pop) and in-memory lookup.
+func Mod5Unsigned(n uint32) uint32 {
+	n = (n >> 16) + (n & 0xFFFF)              // Max 0x1FFFE
+	n = (n >> 8) + (n & 0x00FF)               // Max 0x2FD
+	n = (n >> 4) + (n & 0x000F)               // Max 0x3D
+	n = (n >> 4) - ((n >> 2) & 3) + (n & 3)   // -3 to 6
+	return (01043210432 >> (3 * (n + 3))) & 7 // Octal const
+}
+
+var mod7_unsigned = [...]uint8{
+	0, 1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6,
+	0, 1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6,
+	0, 1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6,
+	0, 1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6,
+	0, 1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6,
+	0, 1, 2, 3, 4,
+}
+
+// Mod7Unsigned (remu7) is using summing method and in-memory lookup.
+func Mod7Unsigned(n uint32) uint32 {
+	n = (n >> 15) + (n & 0x7FFF) // Max 0x27FFE
+	n = (n >> 9) + (n & 0x001FF) // Max 0x33D
+	n = (n >> 6) + (n & 0x0003F) // Max 0x4A
+	return uint32(mod7_unsigned[n])
+}
+
+var mod9_unsigned = [...]uint8{
+	0, 1, 2, 3, 4, 5, 6, 7, 8,
+	0, 1, 2, 3, 4, 5, 6, 7, 8,
+	0, 1, 2, 3, 4, 5, 6, 7, 8,
+	0, 1, 2, 3, 4, 5, 6, 7, 8,
+	0, 1, 2, 3, 4, 5, 6, 7, 8,
+	0, 1, 2, 3, 4, 5, 6, 7, 8,
+	0, 1, 2, 3, 4, 5, 6, 7, 8,
+	0, 1, 2, 3, 4, 5, 6, 7, 8,
+	0, 1, 2,
+}
+
+// Mod9Unsigned (remu9) is using summing method and in-memory lookup.
+func Mod9Unsigned(n uint32) uint32 {
+	r := int32((n & 0x7FFF) - (n >> 15)) // FFFE0001 to 7FFF
+	r = (r & 0x01FF) - (r >> 9)          // FFFFFC1 to 2FF
+	r = (r & 0x003F) + (r >> 6)          // 0 to 4A
+	return uint32(mod9_unsigned[r])
+}
--- 10-19_integer_division_by_constant_reminder_test.go
+++ 10-19_integer_division_by_constant_reminder_test.go
@@ -6,22 +6,26 @@ import (
 	hd "github.com/nikolaydubina/go-hackers-delight"
 )
 
-func FuzzMod3Unsigned(f *testing.F) {
+func FuzzModUnsigned(f *testing.F) {
 	for _, u := range fuzzUint32 {
 		f.Add(u)
 	}
 	f.Fuzz(func(t *testing.T, x uint32) {
-		exp := x % 3
-
-		got := [...]uint32{
-			hd.Mod3Unsigned(x),
-			hd.Mod3Unsigned2(x),
-			hd.Mod3Unsigned3(x),
-			hd.Mod3Unsigned4(x),
+		got := [...]struct {
+			exp uint32
+			got uint32
+		}{
+			{exp: x % 3, got: hd.Mod3Unsigned(x)},
+			{exp: x % 3, got: hd.Mod3Unsigned2(x)},
+			{exp: x % 3, got: hd.Mod3Unsigned3(x)},
+			{exp: x % 3, got: hd.Mod3Unsigned4(x)},
+			{exp: x % 5, got: hd.Mod5Unsigned(x)},
+			{exp: x % 7, got: hd.Mod7Unsigned(x)},
+			{exp: x % 9, got: hd.Mod9Unsigned(x)},
 		}
-		for i, q := range got {
-			if q != exp {
-				t.Errorf("%d: (%d) = %d; want %d", i, x, q, exp)
+		for i, tc := range got {
+			if tc.exp != tc.got {
+				t.Errorf("%d: tc(%v) x(%x)", i, tc, x)
 			}
 		}
 	})
