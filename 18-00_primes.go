c044da6405140d76fe3ecb477950c953a7740986
Nikolay Dubina <nikolay.dubina.pub@gmail.com>
2024-06-14 14:41:19 +0800

    ch18

---
+++ 18-00_primes.go
@@ -0,0 +1,10 @@
+/*
+# Chapter 18: Primes
+
+It is known, that there is no polynomial f(n) that produces a prime for every n.
+*/
+package hd
+
+var InterestingPrimes = [...]uint{
+	(1 << 32) - 5,
+}
